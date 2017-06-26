d0b7c967bb404cde1066cec56733c47012155f72
yybz <kekeru@163.com>
2017-06-26 21:41:35 +0800

    initial commit

---
+++ SnailGirl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnailGirl
+//
+//  Created by Rainbow on 2017/6/26.
+//  Copyright © 2017年 Rainbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
